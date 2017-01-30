af88417b46594fe65d2feb73cced9b8958c40dcf
Emmanuel Gavalda <Manu@MacBook-Pro-de-Emmanuel.local>
2017-01-30 12:13:01 +0100

    start Tp1

---
+++ contacts.js
@@ -0,0 +1,11 @@
+function Contact (id, firstName, lastName, address, phone) {
+  this.id =id;
+  this.firstName =firstName;
+  thi.lastName=lastName;
+  this.address=address;
+  this.phone=phone;
+}
+function MemoryContacts(){
+
+}
+let c = new Contact();
